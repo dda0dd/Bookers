482a75b54a0248887af9da11a0dd4a447f8e8a74
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-05-11 12:50:02 +0000

    [Update] topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
-  get '/top' => 'homes#top'
+  root 'homes#top'
   resources :books
+  get '/books' => 'homes#books'
+  get '/books/220272' => 'books#show'
+  get '/books/220272/edit' => 'books#edit'
+  get '/books/220272' => 'books#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
