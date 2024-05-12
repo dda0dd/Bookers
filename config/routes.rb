b4cb65e231933f2a91c869ef03a9196d03e597dc
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-05-12 12:53:21 +0000

    [Finish] RSpecテスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
   resources :books
-  get '/books' => 'homes#books'
-  get '/books/220272' => 'books#show'
-  get '/books/220272/edit' => 'books#edit'
-  get '/books/220272' => 'books#delete'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
